965f09944b7491c49f47af9d86471fcb021622df
fraukecharms <frharms@gmail.com>
2022-12-19 06:47:49 +0000

    add all tests in testing

--- helper_sagemaker.py
+++ helper_sagemaker.py
@@ -16,45 +16,6 @@ def parse_response(query_response):
     return normalized_boxes, class_names, scores
 
 
-def query_endpoint(endpoint_name="faster-rcnn", image_file_name="Naxos_Taverna.jpg"):
-    # aws_region = boto3.Session().region_name
-
-    # client = boto3.client("sagemaker-runtime", region_name=aws_region)
-
-    with open(image_file_name, "rb") as file:
-        input_img_rb = file.read()
-
-    client = boto3.client("sagemaker-runtime")
-
-    # The MIME type of the input data in the request body.
-    content_type = "application/x-image"
-    # The desired MIME type of the inference in the response.
-    accept = "application/json;verbose;n_predictions=1"
-    # Payload for inference.
-    payload = input_img_rb
-
-    response = client.invoke_endpoint(
-        EndpointName=endpoint_name,
-        ContentType=content_type,
-        Accept=accept,
-        Body=payload,
-    )
-
-    print(response)
-
-    response_readable = response["Body"].read().decode("utf-8")
-
-    # with open("taverna.json", "w") as file:
-    #    json.dump(response_readable, file)
-
-    print(type(response_readable))
-
-    normalized_boxes, class_names, scores = parse_response(response_readable)
-
-    print(class_names)
-    return normalized_boxes, class_names, scores
-
-
 def query_endpoint2(endpoint_name, input_img_rb):
 
     client = boto3.client("sagemaker-runtime")
---
+++ testing/test_helper_sagemaker.py
@@ -0,0 +1,72 @@
+import boto3
+from helper_sagemaker import parse_response
+from PIL import Image, ImageDraw, ImageColor
+from IPython.display import Image as ipImage
+from IPython.display import display as ipdisplay
+from test_main import list_endpoints
+import os
+
+
+
+
+def test_parse_response(endpoint_name="faster-rcnn", image_file_name="testpics/pic3.jpg"):
+    # aws_region = boto3.Session().region_name
+
+    # client = boto3.client("sagemaker-runtime", region_name=aws_region)
+
+    with open(image_file_name, "rb") as file:
+        input_img_rb = file.read()
+
+    client = boto3.client("sagemaker-runtime")
+
+    # The MIME type of the input data in the request body.
+    content_type = "application/x-image"
+    # The desired MIME type of the inference in the response.
+    accept = "application/json;verbose;n_predictions=1"
+    # Payload for inference.
+    payload = input_img_rb
+
+    response = client.invoke_endpoint(
+        EndpointName=endpoint_name,
+        ContentType=content_type,
+        Accept=accept,
+        Body=payload,
+    )
+
+    print(response)
+
+    response_readable = response["Body"].read().decode("utf-8")
+
+    # with open("taverna.json", "w") as file:
+    #    json.dump(response_readable, file)
+
+    print(type(response_readable))
+
+    normalized_boxes, class_names, scores = parse_response(response_readable)
+
+    assert len(normalized_boxes) > 0
+
+
+
+# def test_draw_bounding_boxes():
+
+#     testpic = "testpics/pic3.jpg"
+
+#     client = boto3.client("rekognition")
+
+#     with open(testpic, "rb") as photo:
+
+#         response = client.detect_labels(Image={"Bytes": photo.read()})
+
+#     boxes = process_response(response)
+
+#     photo2 = Image.open(testpic)
+
+#     imgwbox = draw_bounding_boxes3(photo2, boxes[0])
+
+#     if not os.path.exists("images_with_boxes"):
+#         os.mkdir("images_with_boxes")
+#     outpath = "images_with_boxes/pic3_box.jpg"
+#     imgwbox.save(outpath)
+
+#     assert os.path.exists(outpath)
